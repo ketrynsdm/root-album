7734dc98fbd0543ee727ea7ccc93c6fc06f358c1
Ketryn Silva <ketryn29@gmail.com>
2022-05-11 13:14:05 -0300

    first commit

---
+++ footer.php
@@ -0,0 +1,20 @@
+<?php wp_footer(); ?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+<div class="container">
+    <div class="row footer">
+        <div class="col-10 informacao">
+            <p class="frase-footer">
+            Album example is © Bootstrap, but please download and customize it for yourself!
+            New to Bootstrap? Visit the homepage or read our getting started guide.
+            </p>
+        </div>
+        <div class="col-2 home">
+            <a href="http://localhost/wordpress/">Back to top</a>
+        </div>
+    </div>
+
+</div>
+</html>
---
+++ index.php
@@ -0,0 +1,96 @@
+<?php get_header(); ?>
+
+<div class="col container">
+    <h1> Album example </h1>
+    <p id="text">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet,
+        but not too short so folks don’t simply skip over it entirely.
+    </p>
+    <div class="button">
+        <button type="button" class="btn btn-primary">Main call to action</button>
+        <button type="button" class="btn btn-secondary">Secondary action</button>
+    </div>
+
+</div>
+<div class="container center">
+  <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
+    <div class="col">
+      <div class="card" style="width: 100%;">
+        <img src="<?php echo get_template_directory_uri(). '/images/Thumbnail.png' ?>" class="img-thumbnail" alt="...">
+        <div class="card-body">
+          <p class="card-text"  style="color:black;">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <div class="bu">
+          <button id="view" class="view" type="button" class="btn btn-light"  >View</button>
+          <button id="edit" class="edit" type="button" class="btn btn-light">Edit</button>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card" style="width: 100%;">
+        <img src="<?php echo get_template_directory_uri(). '/images/Thumbnail.png' ?>" class="img-thumbnail" alt="...">
+        <div class="card-body">
+          <p class="card-text"  style="color:black;">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <div class="bu">
+          <button id="view" class="view" type="button" class="btn btn-light"  >View</button>
+          <button id="edit" class="edit" type="button" class="btn btn-light">Edit</button>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card" style="width: 100%;">
+        <img src="<?php echo get_template_directory_uri(). '/images/Thumbnail.png' ?>" class="img-thumbnail" alt="...">
+        <div class="card-body">
+          <p class="card-text"  style="color:black;">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <div class="bu">
+          <button id="view" class="view" type="button" class="btn btn-light"  >View</button>
+          <button id="edit" class="edit" type="button" class="btn btn-light">Edit</button>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card" style="width: 100%;">
+        <img src="<?php echo get_template_directory_uri(). '/images/Thumbnail.png' ?>" class="img-thumbnail" alt="...">
+        <div class="card-body">
+          <p class="card-text"  style="color:black;">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <div class="bu">
+          <button id="view" class="view" type="button" class="btn btn-light"  >View</button>
+          <button id="edit" class="edit" type="button" class="btn btn-light">Edit</button>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card" style="width: 100%;">
+        <img src="<?php echo get_template_directory_uri(). '/images/Thumbnail.png' ?>" class="img-thumbnail" alt="...">
+        <div class="card-body">
+          <p class="card-text"  style="color:black;">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <div class="bu">
+          <button id="view" class="view" type="button" class="btn btn-light"  >View</button>
+          <button id="edit" class="edit" type="button" class="btn btn-light">Edit</button>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card" style="width: 100%;">
+        <img src="<?php echo get_template_directory_uri(). '/images/Thumbnail.png' ?>" class="img-thumbnail" alt="...">
+        <div class="card-body">
+          <p class="card-text"  style="color:black;">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <div class="bu">
+          <button id="view" class="view" type="button" class="btn btn-light"  >View</button>
+          <button id="edit" class="edit" type="button" class="btn btn-light">Edit</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+ 
+ 
+
+</div>
+
+
+<?php get_footer(); ?>
